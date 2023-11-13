9f7490a9538405cc7567d23d9b7e5b46888b5535
maxlestage <maxlestage@icloud.com>
2023-11-13 18:10:06 +0100

    BOOM  Chatbot ça fonctionne !

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,63 @@
-use actix_web::{post, App, HttpResponse, HttpServer};
-
+use actix::prelude::*;
+use actix_web::{post, web, App, HttpResponse, HttpServer};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
 #[derive(Serialize, Deserialize)]
 struct AskPrompt {
-    model: String,
     prompt: String,
-    stream: bool,
 }
 
-#[post("/chat")]
-async fn chat() -> HttpResponse {
+#[derive(Deserialize)]
+struct UserChoice {
+    nb_of_ingredients: i32,
+}
+
+struct ChatContext {
+    context: String,
+}
+
+impl Actor for ChatContext {
+    type Context = Context<Self>;
+}
+
+impl ChatContext {
+    fn new() -> Self {
+        Self {
+            context: String::new(),
+        }
+    }
+}
+
+#[derive(Message)]
+#[rtype(result = "String")]
+struct GetContext;
+
+#[derive(Message)]
+#[rtype(result = "()")]
+struct SetContext(String);
+
+impl Handler<GetContext> for ChatContext {
+    type Result = String;
+
+    fn handle(&mut self, _msg: GetContext, _ctx: &mut Context<Self>) -> Self::Result {
+        self.context.clone()
+    }
+}
+
+impl Handler<SetContext> for ChatContext {
+    type Result = ();
+
+    fn handle(&mut self, msg: SetContext, _ctx: &mut Context<Self>) -> Self::Result {
+        self.context = msg.0;
+    }
+}
+#[post("/random-recipe")]
+async fn random_recipe(user_choice: web::Json<UserChoice>) -> HttpResponse {
+    let user_choice: i32 = user_choice.nb_of_ingredients;
+
     let prompt: AskPrompt = AskPrompt {
-        model: "llama2:13b".to_owned(),
-        prompt: "Please provide a Array of 8 ingredients for a dinner recipe. Example:  [\"ingredient1\", \"ingredient2\", \"ingredient3\", \"ingredient4\", \"ingredient5\", \"ingredient6\", \"ingredient7\", \"ingredient8\"]".to_owned(),
-        stream : false // Ollama's API endpoints such as /api/generate now support returning data in one single response. Set the stream parameter to false in the API request:
+        prompt: format!("provide a list of {user_choice} random ingridients for a recipe."),
     };
 
     let prompt_to_json: String = serde_json::to_string(&prompt).unwrap();
@@ -24,7 +66,11 @@ async fn chat() -> HttpResponse {
 
     let res: Result<reqwest::Response, reqwest::Error> = client
         .post("http://localhost:11434/api/generate")
-        .body(prompt_to_json)
+        .json(&serde_json::json!({
+            "model": "orca-mini:3b",
+            "prompt": prompt_to_json,
+            "stream": false,
+        }))
         .send()
         .await;
 
@@ -43,11 +89,55 @@ async fn chat() -> HttpResponse {
     HttpResponse::InternalServerError().body("Error")
 }
 
+#[post("/chat")]
+async fn chat(info: web::Json<AskPrompt>, context: web::Data<Addr<ChatContext>>) -> HttpResponse {
+    let context_addr = context.get_ref().clone();
+    let context = context_addr.send(GetContext).await.unwrap();
+
+    let prompt: AskPrompt = AskPrompt {
+        prompt: format!("{} {}", context, info.prompt),
+    };
+
+    let prompt_to_json: String = serde_json::to_string(&prompt).unwrap();
+
+    let client: reqwest::Client = reqwest::Client::new();
+
+    let res: Result<reqwest::Response, reqwest::Error> = client
+        .post("http://localhost:11434/api/generate")
+        .json(&serde_json::json!({
+            "model": "orca-mini:3b",
+            "prompt": prompt_to_json,
+            "stream": false,
+        }))
+        .send()
+        .await;
+
+    if let Ok(response) = res {
+        let body: String = response
+            .text()
+            .await
+            .unwrap_or_else(|_| "Failed to read response body".to_string());
+
+        let body_parsed_to_json: Value = serde_json::from_str(&body).unwrap();
+        let get_response_from_body: Option<&str> = body_parsed_to_json["response"].as_str();
+
+        context_addr.do_send(SetContext(String::from(get_response_from_body.unwrap())));
+
+        return HttpResponse::Ok().body(String::from(get_response_from_body.unwrap()));
+    }
+
+    HttpResponse::InternalServerError().body("Error")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new().service(chat)
-        // .route("/chat", web::post().to(chat))
+    let context = ChatContext::new().start();
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(context.clone()))
+            .service(random_recipe)
+            .service(chat)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
